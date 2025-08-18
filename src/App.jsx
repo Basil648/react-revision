f7adad63a4d7de2de4f2f01a7fd7552f9f923ef6
Basil648 <basilshajan666@gmail.com>
2025-08-18 10:53:27 +0530

    revised the map function and conditonel rendering using the map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
- import React from 'react'
- 
- function App() {
-  let a=5;
-  let b=6;
-   return (
-     <>
-     
-     {a<5?"a < 5":"a>5"}
-     </>
-   )
- }
- 
- export default App
+import React from "react";
+
+function App() {
+  const fruits = [
+    { id: 1, name: "Apple", color: "Red" },
+    { id: 2, name: "Banana", color: "Yellow" },
+    { id: 3, name: "Orange", color: "Orange" },
+    { id: 4, name: "Grapes", color: "Purple" },
+    { id: 5, name: "Mango", color: "Yellow" }
+  ];
+
+  return (
+    <>
+      <h2>Fruit List</h2>
+      <ul>
+        {fruits.map((fruit) => (
+          fruit.color === "Yellow" ? (
+            <li key={fruit.id}>{fruit.name} is yello</li>
+          ) : null
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
